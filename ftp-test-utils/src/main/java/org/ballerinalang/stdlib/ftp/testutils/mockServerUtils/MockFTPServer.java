9cc179b69d7423e9f340ff307a4aff85e4e256f7
Maninda Edirisooriya <mwe163@gmail.com>
2021-06-14 09:41:20 +0530

    Apply suggestions from code review
    Co-authored-by: praneesha <praneesha@wso2.com>
    Co-authored-by: Chanaka Lakmal <ldclakmal@gmail.com>

--- ftp-test-utils/src/main/java/org/ballerinalang/stdlib/ftp/testutils/mockServerUtils/MockFTPServer.java
+++ ftp-test-utils/src/main/java/org/ballerinalang/stdlib/ftp/testutils/mockServerUtils/MockFTPServer.java
@@ -262,7 +262,6 @@ public class MockFTPServer {
             }
         }
         logger.info("Started Mock SFTP server");
-
     }
 
     private static KeyPairProvider getKeyPairProvider() throws IOException, CertificateException, NoSuchAlgorithmException,
